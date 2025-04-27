562574b8197df2e901057cdc6bb0d4d7941dea25
WesleySouza13 <souzabusiness13@gmail.com>
2025-04-27 15:44:18 -0300

    criando .csv cluster

--- model/train.py
+++ model/train.py
@@ -6,4 +6,9 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+# %%
+df = pd.read_csv('C:\\Users\\souza\\Downloads\\projeto credito\\data\\dataset_cluster.csv')
+# %%
 # %%
