c1912856a725ef6da4bce3230dec6a8c559a6efb
WesleySouza13 <souzabusiness13@gmail.com>
2025-04-27 14:43:13 -0300

    criando arquivo train.py

---
+++ model/train.py
@@ -0,0 +1,9 @@
+# %%
+import pandas as pd 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+# %%
